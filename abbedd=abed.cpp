dd0d71a5f81d507d0e7d70c4dd43b62d0bf876aa
Thiraphat Ketsingnoi <63010467@kmitl.ac.th>
2020-10-19 12:00:25 +0700

    onechar

---
+++ abbedd=abed.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void set(char*);
+
+int main()
+{
+	char y[50];
+	char* py = y ;
+	scanf("%s", y);
+	set(py);
+	return 0;
+}
+void set(char *a)
+{
+	int x = 0;
+	while (*(a+x) != '\0')
+	{
+		if (*(a+x) != *(a+x+1))
+		{
+			printf("%c", *(a+x));
+		}
+		x++;
+	}
+}
